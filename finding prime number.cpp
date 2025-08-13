bc1b2ef1dd2e125be2c6fc2f0a5cb9da0462a680
HAMMAD UR REHMAN <hammadurrehman957@gmail.com>
2025-08-13 09:38:03 +0500

    Add files via upload

--- finding prime number.cpp
+++ finding prime number.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int main() {
       
-  int n=7;
+  int n;
+  cout<<"enter a number to check wether it's a prime number or not"<<endl;
+  cin>>n;
+
 
   bool isPrime=true;
   
-  for (int i =2 ;i<=n-1 ;i++){
+  for (int i =2 ;i*i<=n ;i++){
     if (n%i==0){
       isPrime=false;
       break;
